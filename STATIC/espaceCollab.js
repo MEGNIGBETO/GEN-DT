bd47fd68a36284d336e3cc37117decbd7e54fbda
MEGNIGBETO <souvenirmegnigbeto@gmail.com>
2024-07-02 17:16:15 +0200

    ajout espaceCollab

---
+++ STATIC/espaceCollab.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $.getJSON('DATA/espaceCollab.json', function(data) {
+        $.each(data, function(index, espaceCollab) {
+          var espaceCollabElement = `
+            <div class="titre-collab">${espaceCollab.titre}</div>
+            <img class="image" src="${espaceCollab.image}" alt="">
+            <a class="inscriptionCollab" href="${espaceCollab.formulaire}">S'inscrire</a>
+            <div class="logo-organisation">
+                <img src="${espaceCollab.logoGenDT}" alt="">
+                <img src="${espaceCollab.logoPartner}" alt="">
+            </div>
+          `;
+          $('.collab').append(espaceCollabElement);
+        });
+    });  
+})
