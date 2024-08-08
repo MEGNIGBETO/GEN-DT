ddcbc128781842a6135a599dba8c847798b38dd8
MEGNIGBETO <souvenirmegnigbeto@gmail.com>
2024-08-08 16:36:17 +0200

    liaison gendt et gendtloisir

---
+++ DEPARTEMENT/departement.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $.getJSON('DEPARTEMENT/departement.json', function(data) {
+        $.each(data, function(index, departement) {
+            $('#departement').append(`
+                <div class="departement" id="${departement.id}">
+                    <div class="departement-couverture">
+                        <img  src="${departement.couverture}" alt="photo de couverture de ${departement.name}">
+                    </div>
+                    <div class="departement-info">
+                        <h3>${departement.name}</h3>
+                        <p>Visitez notre plateforme dédiée <a href="${departement.plateforme}">ici</a></p>
+                    </div>
+                </div>
+            `);
+        });
+    });
+});
--- STATIC/espaceCollab.js
+++ STATIC/espaceCollab.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
           var espaceCollabElement = `
             <div class="titre-collab">${espaceCollab.titre}</div>
             <img class="image" src="${espaceCollab.image}" alt="">
-            <a class="inscriptionCollab" href="${espaceCollab.formulaire}">S'inscrire</a>
+            <!--<a class="inscriptionCollab" href="${espaceCollab.formulaire}">S'inscrire</a>-->
             <div class="logo-organisation">
                 <img src="${espaceCollab.logoGenDT}" alt="">
                 <img src="${espaceCollab.logoPartner}" alt="">
